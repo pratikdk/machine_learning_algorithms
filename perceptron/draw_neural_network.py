087c09fb0dc8aa0b55e903b70a2a63d60fd4d2c6
unknown <pratik.deoolwadikar@gmail.com>
2020-04-02 22:48:25 +0530

    Daily update

---
+++ perceptron/draw_neural_network.py
@@ -0,0 +1,63 @@
+## Credits
+# https://gist.github.com/craffel
+# Matplotlib NeuralNet plotting code
+
+import matplotlib.pyplot as plt
+
+def draw(ax, left, right, bottom, top, layer_sizes, activations):
+    '''
+    Draw a neural network cartoon using matplotilb.
+    
+    :usage:
+        >>> fig = plt.figure(figsize=(12, 12))
+        >>> draw_neural_net(fig.gca(), .1, .9, .1, .9, [4, 7, 2])
+    
+    :parameters:
+        - ax : matplotlib.axes.AxesSubplot
+            The axes on which to plot the cartoon (get e.g. by plt.gca())
+        - left : float
+            The center of the leftmost node(s) will be placed here
+        - right : float
+            The center of the rightmost node(s) will be placed here
+        - bottom : float
+            The center of the bottommost node(s) will be placed here
+        - top : float
+            The center of the topmost node(s) will be placed here
+        - layer_sizes : list of int
+            List of layer sizes, including input and output dimensionality
+    '''
+    n_layers = len(layer_sizes)
+    v_spacing = (top - bottom)/float(max(layer_sizes))
+    h_spacing = (right - left)/float(len(layer_sizes) - 1)
+    # Nodes
+    for n, layer_size in enumerate(layer_sizes):
+        layer_top = (v_spacing*(layer_size - 1)/2. + (top + bottom)/2.)
+        for m in range(layer_size):
+            node_color = "#b3f2ff"
+            text_color = "#000000"
+
+            if (n == 0):
+                node_color = "#c9c9c9"
+            elif (n == len(layer_sizes)-1):
+                node_color = "#000000"
+                text_color = "#ffffff"
+            
+            circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4., color=node_color, ec='k', zorder=4)
+            ax.add_artist(circle)
+
+            if activations:
+                if (n > 0) & (n < len(layer_sizes)-1): ax.annotate("0.12", xy=(n*h_spacing + left, layer_top - m*v_spacing), size=10, color=text_color, ha="center", zorder=5, verticalalignment='center', horizontalalignment='center')
+            
+            ax.axis('off')
+            ax.set_aspect('equal')
+    # Edges
+    for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+        layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
+        layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
+        for m in range(layer_size_a):
+            for o in range(layer_size_b):
+                line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
+                                  [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
+                ax.add_artist(line)
+
+    plt.show()
